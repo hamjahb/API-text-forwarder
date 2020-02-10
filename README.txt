c1716e37791bf799d2cadeafe18179b7751e8c45
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-02-10 10:19:28 +0300

    base server

---
+++ README.txt
@@ -0,0 +1,3 @@
+http://10.51.1.23:3000/?phone=0780291130&text=hi there 1
+http://10.51.1.23:3000/?phone=<phone>&text=<text>
+https://chat.whatsapp.com/JABTE6af5wx2eCXglPzp57
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+let bodyParser = require('body-parser');
+// let db = require("../database/db");
+let app = express();
+
+let array = [];
+// the problem was CORS origin and the request sent to port 3000 [Front-end port] not 5000 [Back-end port]
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  console.log('GET SMS');
+  console.log(req.query);
+  const { phone, text } = req.query;
+  array.push({ phone, text });
+  res.send('OK');
+});
+app.post('/', (req, res) => {
+  console.log('POST SMS');
+  console.log(req.query);
+  res.send('OK');
+});
+
+app.get('/all', (req, res) => {
+  console.log('GET ALL SMS');
+  console.log(req.query);
+  const { phone, text } = req.query;
+  array.push({ phone, text });
+  // res.json('sms length: ' + array.length);
+  res.send(array);
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', function() {
+  console.log(`listening on port ${PORT}!`);
+});
