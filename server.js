b665a1868b157eeafe29609403d24c9847205ce4
Hisham Aljahbli <h.aljahbli@gmail.com>
2021-03-08 21:58:21 +0300

    add EServices

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ MOROOR(Traffic)                                 : 'MOI-MOROOR'
 Mudad                                           : 'Mudad'
 
 Muqeem                                          : 'RAYA'
+                                                : 'EServices'
 
 Nafath                                          : 'IAM.GOV.SA'
 
@@ -105,7 +106,7 @@ Saudi Telecom Company (STC)                     : 'mystc'
 Amazon Saudi Arabia                             : 'Amazon'
 */
 
-const lowerCaseAllowed = ['fasah', 'customs', 'mc.gov.sa', 'moi-moroor','nwc', 'anb', 'saudi post', 'moj.gov.sa', 'gazt.gov.sa', 'gosi', 'moi.gov.sa', 'iam.gov.sa', 'alinmabank', 'alahlisms', 'riyadbank', 'alrajhibank', 'ruh_chamber', 'mystc', 'stcbusiness', 'alkahraba', 'momra', 'raya', 'aljazirasms', 'simah', 'mudad', 'qiwa', '998.salamah', 'amazon', 'saber', 'riyadhmun', 'm-code', '+972123456789', '+001-123456789']
+const lowerCaseAllowed = ['fasah', 'customs', 'mc.gov.sa', 'moi-moroor','nwc', 'anb', 'saudi post', 'moj.gov.sa', 'gazt.gov.sa', 'gosi', 'moi.gov.sa', 'iam.gov.sa', 'alinmabank', 'alahlisms', 'riyadbank', 'alrajhibank', 'ruh_chamber', 'mystc', 'stcbusiness', 'alkahraba', 'momra', 'raya', 'aljazirasms', 'simah', 'mudad', 'qiwa', '998.salamah', 'amazon', 'saber', 'riyadhmun', 'm-code', 'eservices', '+972123456789', '+001-123456789']
 
 function codeFilter(smsToFilter) {  
   lowerCasePhone = smsToFilter.toLowerCase()
