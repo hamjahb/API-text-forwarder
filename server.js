7e0143ad35f4a247a6f6f77ae367f80a35a378f6
Hisham Aljahbli <h.aljahbli@gmail.com>
2021-06-09 12:16:22 +0300

    add ajeer to permited list

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ Aljazira Bank       : 'AlJaziraSMS'
 
 
 *** GOVERNMENT INSTITUTIONS ***
+Ajeer                                           :'AJEER-HRSD'
+
 Fasah                                           : 'FASAH'
 
 General Authority of Zakat and Tax              : 'Gazt.gov.sa'
@@ -107,8 +109,10 @@ Saudi Telecom Company (STC)                     :'mystc'
 Amazon Saudi Arabia                             : 'Amazon'
 */
 
-const lowerCaseAllowed = ['fasah', 'customs', 'mc.gov.sa', 'moi-moroor','nwc', 'anb', 'saudi post', 'moj.gov.sa', 'gazt.gov.sa', 'gosi', 'moi.gov.sa', 'iam.gov.sa', 'alinmabank', 'alahlisms', 'riyadbank', 'alrajhibank', 'ruh_chamber', 'mystc', 'stcbusiness', 'alkahraba', 'momra', 'raya', 'aljazirasms', 'simah', 'mudad', 'qiwa', '998.salamah', 'amazon', 'saber', 'riyadhmun', 'm-code', 'eservices', '+972123456789', '+001-123456789']
+const lowerCaseAllowed = ['fasah', 'customs', 'mc.gov.sa', 'moi-moroor','nwc', 'anb', 'saudi post', 'moj.gov.sa', 'gazt.gov.sa', 'gosi', 'moi.gov.sa', 'iam.gov.sa', 'alinmabank', 'alahlisms', 'riyadbank', 'alrajhibank', 'ruh_chamber', 'mystc', 'stcbusiness', 'alkahraba', 'momra', 'raya', 'aljazirasms', 'simah', 'mudad', 'qiwa', '998.salamah', 'amazon', 'saber', 'riyadhmun', 'm-code', 'eservices','ajeer-hrsd', '+972123456789', '+001-123456789']
 
+
+// standerdize sms then filter
 function codeFilter(smsToFilter) {  
   lowerCasePhone = smsToFilter.toLowerCase()
   console.log(lowerCasePhone);
