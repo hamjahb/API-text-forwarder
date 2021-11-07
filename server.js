66ec41540ad1434363cbc5848b54db200c3db811
Hisham Aljahbli <h.aljahbli@gmail.com>
2021-11-07 12:11:46 +0300

    add STC out ER

--- server.js
+++ server.js
@@ -112,6 +112,7 @@ Saudi Electricity Company (Alkahraba)           :'ALKAHRABA'
 
 Saudi Telecom Company (STC)                     :'mystc'
                                                 :'stcbusiness'
+                                                :'Stc Out ER'
 
 
 *** THIRD PARTY COMPANIES ****
@@ -122,7 +123,7 @@ dnet                                            : 'dnet'
 obiky system                                    : 'M-CODE'
 */
 
-const lowerCaseAllowed = ['fasah', 'customs', 'mc.gov.sa', 'moi-moroor','nwc', 'anb', 'saudi post', 'moj.gov.sa', 'gazt.gov.sa', 'gosi', 'moi.gov.sa', 'iam.gov.sa', 'alinmabank', 'alahlisms', 'riyadbank', 'alrajhibank', 'ruh_chamber', 'mystc', 'stcbusiness', 'alkahraba', 'momra', 'raya', 'aljazirasms', 'simah', 'mudad', 'qiwa', '998.salamah', 'amazon', 'saber', 'riyadhmun', 'm-code', 'eservices','ajeer-hrsd','zatca', 'snb-alahli','FASAH.SA', 'dnet', 'm-code', '+972123456789', '+001-123456789']
+const lowerCaseAllowed = ['fasah', 'customs', 'mc.gov.sa', 'moi-moroor','nwc', 'anb', 'saudi post', 'moj.gov.sa', 'gazt.gov.sa', 'gosi', 'moi.gov.sa', 'iam.gov.sa', 'alinmabank', 'alahlisms', 'riyadbank', 'alrajhibank', 'ruh_chamber', 'mystc', 'stcbusiness', 'alkahraba', 'momra', 'raya', 'aljazirasms', 'simah', 'mudad', 'qiwa', '998.salamah', 'amazon', 'saber', 'riyadhmun', 'm-code', 'eservices','ajeer-hrsd','zatca', 'snb-alahli','FASAH.SA', 'dnet', 'm-code', 'Stc Out ER', '+972123456789', '+001-123456789']
 
 
 // standerdize sms then filter
